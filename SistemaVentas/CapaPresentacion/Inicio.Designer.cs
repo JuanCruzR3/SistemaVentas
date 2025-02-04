4661abaa46c159454abcf48c7b9e7e339abad797
Juan <juancruzrodriguez3@gmail.com>
2025-02-04 15:55:53 -0300

    arreglos de diseño

--- SistemaVentas/CapaPresentacion/Inicio.Designer.cs
+++ SistemaVentas/CapaPresentacion/Inicio.Designer.cs
@@ -28,12 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            menu = new MenuStrip();
+            menutitulo = new MenuStrip();
+            label1 = new Label();
+            menuacercade = new FontAwesome.Sharp.IconMenuItem();
+            menureportes = new FontAwesome.Sharp.IconMenuItem();
+            menuproveedores = new FontAwesome.Sharp.IconMenuItem();
+            menuclientes = new FontAwesome.Sharp.IconMenuItem();
+            menucompras = new FontAwesome.Sharp.IconMenuItem();
+            menuventas = new FontAwesome.Sharp.IconMenuItem();
+            menumantenedor = new FontAwesome.Sharp.IconMenuItem();
+            menuusuarios = new FontAwesome.Sharp.IconMenuItem();
+            menu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menu
+            // 
+            menu.BackColor = Color.White;
+            menu.Items.AddRange(new ToolStripItem[] { menuusuarios, menumantenedor, menuventas, menucompras, menuclientes, menuproveedores, menureportes, menuacercade });
+            menu.Location = new Point(0, 58);
+            menu.Name = "menu";
+            menu.Size = new Size(800, 73);
+            menu.TabIndex = 0;
+            menu.Text = "menuStrip1";
+            // 
+            // menutitulo
+            // 
+            menutitulo.AutoSize = false;
+            menutitulo.BackColor = Color.SteelBlue;
+            menutitulo.Location = new Point(0, 0);
+            menutitulo.Name = "menutitulo";
+            menutitulo.RightToLeft = RightToLeft.Yes;
+            menutitulo.Size = new Size(800, 58);
+            menutitulo.TabIndex = 1;
+            menutitulo.Text = "menuStrip2";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.SteelBlue;
+            label1.Font = new Font("Verdana", 20.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(8, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(241, 32);
+            label1.TabIndex = 2;
+            label1.Text = "Super Primavera";
+            // 
+            // menuacercade
+            // 
+            menuacercade.IconChar = FontAwesome.Sharp.IconChar.InfoCircle;
+            menuacercade.IconColor = Color.Black;
+            menuacercade.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menuacercade.IconSize = 50;
+            menuacercade.ImageScaling = ToolStripItemImageScaling.None;
+            menuacercade.Name = "menuacercade";
+            menuacercade.Size = new Size(72, 69);
+            menuacercade.Text = "Acerca De";
+            menuacercade.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menureportes
+            // 
+            menureportes.IconChar = FontAwesome.Sharp.IconChar.ChartBar;
+            menureportes.IconColor = Color.Black;
+            menureportes.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menureportes.IconSize = 50;
+            menureportes.ImageScaling = ToolStripItemImageScaling.None;
+            menureportes.Name = "menureportes";
+            menureportes.Size = new Size(65, 69);
+            menureportes.Text = "Reportes";
+            menureportes.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menuproveedores
+            // 
+            menuproveedores.IconChar = FontAwesome.Sharp.IconChar.AddressCard;
+            menuproveedores.IconColor = Color.Black;
+            menuproveedores.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menuproveedores.IconSize = 50;
+            menuproveedores.ImageScaling = ToolStripItemImageScaling.None;
+            menuproveedores.Name = "menuproveedores";
+            menuproveedores.Size = new Size(84, 69);
+            menuproveedores.Text = "Proveedores";
+            menuproveedores.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menuclientes
+            // 
+            menuclientes.IconChar = FontAwesome.Sharp.IconChar.UserFriends;
+            menuclientes.IconColor = Color.Black;
+            menuclientes.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menuclientes.IconSize = 50;
+            menuclientes.ImageScaling = ToolStripItemImageScaling.None;
+            menuclientes.Name = "menuclientes";
+            menuclientes.Size = new Size(62, 69);
+            menuclientes.Text = "Clientes";
+            menuclientes.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menucompras
+            // 
+            menucompras.IconChar = FontAwesome.Sharp.IconChar.DollyFlatbed;
+            menucompras.IconColor = Color.Black;
+            menucompras.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menucompras.IconSize = 50;
+            menucompras.ImageScaling = ToolStripItemImageScaling.None;
+            menucompras.Name = "menucompras";
+            menucompras.Size = new Size(67, 69);
+            menucompras.Text = "Compras";
+            menucompras.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menuventas
+            // 
+            menuventas.IconChar = FontAwesome.Sharp.IconChar.Tags;
+            menuventas.IconColor = Color.Black;
+            menuventas.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menuventas.IconSize = 50;
+            menuventas.ImageScaling = ToolStripItemImageScaling.None;
+            menuventas.Name = "menuventas";
+            menuventas.Size = new Size(62, 69);
+            menuventas.Text = "Ventas";
+            menuventas.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menumantenedor
+            // 
+            menumantenedor.IconChar = FontAwesome.Sharp.IconChar.Tools;
+            menumantenedor.IconColor = Color.Black;
+            menumantenedor.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menumantenedor.IconSize = 50;
+            menumantenedor.ImageScaling = ToolStripItemImageScaling.None;
+            menumantenedor.Name = "menumantenedor";
+            menumantenedor.Size = new Size(84, 69);
+            menumantenedor.Text = "Mantenedor";
+            menumantenedor.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menuusuarios
+            // 
+            menuusuarios.IconChar = FontAwesome.Sharp.IconChar.UsersCog;
+            menuusuarios.IconColor = Color.Black;
+            menuusuarios.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menuusuarios.IconSize = 50;
+            menuusuarios.ImageScaling = ToolStripItemImageScaling.None;
+            menuusuarios.Name = "menuusuarios";
+            menuusuarios.Size = new Size(64, 69);
+            menuusuarios.Text = "Usuarios";
+            menuusuarios.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // Inicio
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(label1);
+            Controls.Add(menu);
+            Controls.Add(menutitulo);
+            MainMenuStrip = menu;
+            Name = "Inicio";
+            Text = "Menu";
+            menu.ResumeLayout(false);
+            menu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menu;
+        private MenuStrip menutitulo;
+        private FontAwesome.Sharp.IconMenuItem menuacercade;
+        private Label label1;
+        private FontAwesome.Sharp.IconMenuItem menuusuarios;
+        private FontAwesome.Sharp.IconMenuItem menumantenedor;
+        private FontAwesome.Sharp.IconMenuItem menuventas;
+        private FontAwesome.Sharp.IconMenuItem menucompras;
+        private FontAwesome.Sharp.IconMenuItem menuclientes;
+        private FontAwesome.Sharp.IconMenuItem menuproveedores;
+        private FontAwesome.Sharp.IconMenuItem menureportes;
     }
 }
