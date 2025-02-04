990ca81590787b0329509a0611b7ae1634d4ec1f
Juan <juancruzrodriguez3@gmail.com>
2025-02-04 18:40:40 -0300

    conexion a base de datos y otros agregados

--- SistemaVentas/CapaPresentacion/Inicio.Designer.cs
+++ SistemaVentas/CapaPresentacion/Inicio.Designer.cs
@@ -29,16 +29,17 @@
         private void InitializeComponent()
         {
             menu = new MenuStrip();
+            menuusuarios = new FontAwesome.Sharp.IconMenuItem();
+            menumantenedor = new FontAwesome.Sharp.IconMenuItem();
+            menuventas = new FontAwesome.Sharp.IconMenuItem();
+            menucompras = new FontAwesome.Sharp.IconMenuItem();
+            menuclientes = new FontAwesome.Sharp.IconMenuItem();
+            menuproveedores = new FontAwesome.Sharp.IconMenuItem();
+            menureportes = new FontAwesome.Sharp.IconMenuItem();
+            menuacercade = new FontAwesome.Sharp.IconMenuItem();
             menutitulo = new MenuStrip();
             label1 = new Label();
-            menuacercade = new FontAwesome.Sharp.IconMenuItem();
-            menureportes = new FontAwesome.Sharp.IconMenuItem();
-            menuproveedores = new FontAwesome.Sharp.IconMenuItem();
-            menuclientes = new FontAwesome.Sharp.IconMenuItem();
-            menucompras = new FontAwesome.Sharp.IconMenuItem();
-            menuventas = new FontAwesome.Sharp.IconMenuItem();
-            menumantenedor = new FontAwesome.Sharp.IconMenuItem();
-            menuusuarios = new FontAwesome.Sharp.IconMenuItem();
+            contenedor = new Panel();
             menu.SuspendLayout();
             SuspendLayout();
             // 
@@ -48,92 +49,33 @@
             menu.Items.AddRange(new ToolStripItem[] { menuusuarios, menumantenedor, menuventas, menucompras, menuclientes, menuproveedores, menureportes, menuacercade });
             menu.Location = new Point(0, 58);
             menu.Name = "menu";
-            menu.Size = new Size(800, 73);
+            menu.Size = new Size(976, 73);
             menu.TabIndex = 0;
             menu.Text = "menuStrip1";
             // 
-            // menutitulo
+            // menuusuarios
             // 
-            menutitulo.AutoSize = false;
-            menutitulo.BackColor = Color.SteelBlue;
-            menutitulo.Location = new Point(0, 0);
-            menutitulo.Name = "menutitulo";
-            menutitulo.RightToLeft = RightToLeft.Yes;
-            menutitulo.Size = new Size(800, 58);
-            menutitulo.TabIndex = 1;
-            menutitulo.Text = "menuStrip2";
+            menuusuarios.IconChar = FontAwesome.Sharp.IconChar.UsersGear;
+            menuusuarios.IconColor = Color.Black;
+            menuusuarios.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menuusuarios.IconSize = 50;
+            menuusuarios.ImageScaling = ToolStripItemImageScaling.None;
+            menuusuarios.Name = "menuusuarios";
+            menuusuarios.Size = new Size(64, 69);
+            menuusuarios.Text = "Usuarios";
+            menuusuarios.TextImageRelation = TextImageRelation.ImageAboveText;
             // 
-            // label1
+            // menumantenedor
             // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.SteelBlue;
-            label1.Font = new Font("Verdana", 20.25F, FontStyle.Italic, GraphicsUnit.Point);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(8, 8);
-            label1.Name = "label1";
-            label1.Size = new Size(241, 32);
-            label1.TabIndex = 2;
-            label1.Text = "Super Primavera";
-            // 
-            // menuacercade
-            // 
-            menuacercade.IconChar = FontAwesome.Sharp.IconChar.InfoCircle;
-            menuacercade.IconColor = Color.Black;
-            menuacercade.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            menuacercade.IconSize = 50;
-            menuacercade.ImageScaling = ToolStripItemImageScaling.None;
-            menuacercade.Name = "menuacercade";
-            menuacercade.Size = new Size(72, 69);
-            menuacercade.Text = "Acerca De";
-            menuacercade.TextImageRelation = TextImageRelation.ImageAboveText;
-            // 
-            // menureportes
-            // 
-            menureportes.IconChar = FontAwesome.Sharp.IconChar.ChartBar;
-            menureportes.IconColor = Color.Black;
-            menureportes.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            menureportes.IconSize = 50;
-            menureportes.ImageScaling = ToolStripItemImageScaling.None;
-            menureportes.Name = "menureportes";
-            menureportes.Size = new Size(65, 69);
-            menureportes.Text = "Reportes";
-            menureportes.TextImageRelation = TextImageRelation.ImageAboveText;
-            // 
-            // menuproveedores
-            // 
-            menuproveedores.IconChar = FontAwesome.Sharp.IconChar.AddressCard;
-            menuproveedores.IconColor = Color.Black;
-            menuproveedores.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            menuproveedores.IconSize = 50;
-            menuproveedores.ImageScaling = ToolStripItemImageScaling.None;
-            menuproveedores.Name = "menuproveedores";
-            menuproveedores.Size = new Size(84, 69);
-            menuproveedores.Text = "Proveedores";
-            menuproveedores.TextImageRelation = TextImageRelation.ImageAboveText;
-            // 
-            // menuclientes
-            // 
-            menuclientes.IconChar = FontAwesome.Sharp.IconChar.UserFriends;
-            menuclientes.IconColor = Color.Black;
-            menuclientes.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            menuclientes.IconSize = 50;
-            menuclientes.ImageScaling = ToolStripItemImageScaling.None;
-            menuclientes.Name = "menuclientes";
-            menuclientes.Size = new Size(62, 69);
-            menuclientes.Text = "Clientes";
-            menuclientes.TextImageRelation = TextImageRelation.ImageAboveText;
-            // 
-            // menucompras
-            // 
-            menucompras.IconChar = FontAwesome.Sharp.IconChar.DollyFlatbed;
-            menucompras.IconColor = Color.Black;
-            menucompras.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            menucompras.IconSize = 50;
-            menucompras.ImageScaling = ToolStripItemImageScaling.None;
-            menucompras.Name = "menucompras";
-            menucompras.Size = new Size(67, 69);
-            menucompras.Text = "Compras";
-            menucompras.TextImageRelation = TextImageRelation.ImageAboveText;
+            menumantenedor.IconChar = FontAwesome.Sharp.IconChar.ScrewdriverWrench;
+            menumantenedor.IconColor = Color.Black;
+            menumantenedor.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menumantenedor.IconSize = 50;
+            menumantenedor.ImageScaling = ToolStripItemImageScaling.None;
+            menumantenedor.Name = "menumantenedor";
+            menumantenedor.Size = new Size(84, 69);
+            menumantenedor.Text = "Mantenedor";
+            menumantenedor.TextImageRelation = TextImageRelation.ImageAboveText;
             // 
             // menuventas
             // 
@@ -147,40 +89,109 @@
             menuventas.Text = "Ventas";
             menuventas.TextImageRelation = TextImageRelation.ImageAboveText;
             // 
-            // menumantenedor
+            // menucompras
             // 
-            menumantenedor.IconChar = FontAwesome.Sharp.IconChar.Tools;
-            menumantenedor.IconColor = Color.Black;
-            menumantenedor.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            menumantenedor.IconSize = 50;
-            menumantenedor.ImageScaling = ToolStripItemImageScaling.None;
-            menumantenedor.Name = "menumantenedor";
-            menumantenedor.Size = new Size(84, 69);
-            menumantenedor.Text = "Mantenedor";
-            menumantenedor.TextImageRelation = TextImageRelation.ImageAboveText;
+            menucompras.IconChar = FontAwesome.Sharp.IconChar.DollyFlatbed;
+            menucompras.IconColor = Color.Black;
+            menucompras.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menucompras.IconSize = 50;
+            menucompras.ImageScaling = ToolStripItemImageScaling.None;
+            menucompras.Name = "menucompras";
+            menucompras.Size = new Size(67, 69);
+            menucompras.Text = "Compras";
+            menucompras.TextImageRelation = TextImageRelation.ImageAboveText;
             // 
-            // menuusuarios
+            // menuclientes
             // 
-            menuusuarios.IconChar = FontAwesome.Sharp.IconChar.UsersCog;
-            menuusuarios.IconColor = Color.Black;
-            menuusuarios.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            menuusuarios.IconSize = 50;
-            menuusuarios.ImageScaling = ToolStripItemImageScaling.None;
-            menuusuarios.Name = "menuusuarios";
-            menuusuarios.Size = new Size(64, 69);
-            menuusuarios.Text = "Usuarios";
-            menuusuarios.TextImageRelation = TextImageRelation.ImageAboveText;
+            menuclientes.IconChar = FontAwesome.Sharp.IconChar.UserGroup;
+            menuclientes.IconColor = Color.Black;
+            menuclientes.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menuclientes.IconSize = 50;
+            menuclientes.ImageScaling = ToolStripItemImageScaling.None;
+            menuclientes.Name = "menuclientes";
+            menuclientes.Size = new Size(62, 69);
+            menuclientes.Text = "Clientes";
+            menuclientes.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menuproveedores
+            // 
+            menuproveedores.IconChar = FontAwesome.Sharp.IconChar.AddressCard;
+            menuproveedores.IconColor = Color.Black;
+            menuproveedores.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menuproveedores.IconSize = 50;
+            menuproveedores.ImageScaling = ToolStripItemImageScaling.None;
+            menuproveedores.Name = "menuproveedores";
+            menuproveedores.Size = new Size(84, 69);
+            menuproveedores.Text = "Proveedores";
+            menuproveedores.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menureportes
+            // 
+            menureportes.IconChar = FontAwesome.Sharp.IconChar.ChartBar;
+            menureportes.IconColor = Color.Black;
+            menureportes.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menureportes.IconSize = 50;
+            menureportes.ImageScaling = ToolStripItemImageScaling.None;
+            menureportes.Name = "menureportes";
+            menureportes.Size = new Size(65, 69);
+            menureportes.Text = "Reportes";
+            menureportes.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menuacercade
+            // 
+            menuacercade.IconChar = FontAwesome.Sharp.IconChar.CircleInfo;
+            menuacercade.IconColor = Color.Black;
+            menuacercade.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            menuacercade.IconSize = 50;
+            menuacercade.ImageScaling = ToolStripItemImageScaling.None;
+            menuacercade.Name = "menuacercade";
+            menuacercade.Size = new Size(72, 69);
+            menuacercade.Text = "Acerca De";
+            menuacercade.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // menutitulo
+            // 
+            menutitulo.AutoSize = false;
+            menutitulo.BackColor = Color.SteelBlue;
+            menutitulo.Location = new Point(0, 0);
+            menutitulo.Name = "menutitulo";
+            menutitulo.RightToLeft = RightToLeft.Yes;
+            menutitulo.Size = new Size(976, 58);
+            menutitulo.TabIndex = 1;
+            menutitulo.Text = "menuStrip2";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.SteelBlue;
+            label1.Font = new Font("Verdana", 20.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(8, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(273, 32);
+            label1.TabIndex = 2;
+            label1.Text = "SUPER PRIMAVERA";
+            // 
+            // contenedor
+            // 
+            contenedor.Dock = DockStyle.Fill;
+            contenedor.Location = new Point(0, 131);
+            contenedor.Name = "contenedor";
+            contenedor.Size = new Size(976, 370);
+            contenedor.TabIndex = 3;
             // 
             // Inicio
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(976, 501);
+            Controls.Add(contenedor);
             Controls.Add(label1);
             Controls.Add(menu);
             Controls.Add(menutitulo);
             MainMenuStrip = menu;
             Name = "Inicio";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Menu";
             menu.ResumeLayout(false);
             menu.PerformLayout();
@@ -201,5 +212,6 @@
         private FontAwesome.Sharp.IconMenuItem menuclientes;
         private FontAwesome.Sharp.IconMenuItem menuproveedores;
         private FontAwesome.Sharp.IconMenuItem menureportes;
+        private Panel contenedor;
     }
 }
---
+++ SistemaVentas/CapaPresentacion/Login.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace CapaPresentacion
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            txtdocumento = new TextBox();
+            txtclave = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            btningresar = new FontAwesome.Sharp.IconButton();
+            btncancelar = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.SteelBlue;
+            label1.Dock = DockStyle.Left;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(206, 193);
+            label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.SteelBlue;
+            label2.Font = new Font("Verdana", 12.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(12, 141);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 20);
+            label2.TabIndex = 1;
+            label2.Text = "SUPER PRIMAVERA";
+            // 
+            // iconPictureBox1
+            // 
+            iconPictureBox1.BackColor = Color.SteelBlue;
+            iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Store;
+            iconPictureBox1.IconColor = Color.White;
+            iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconPictureBox1.IconSize = 115;
+            iconPictureBox1.Location = new Point(46, 23);
+            iconPictureBox1.Name = "iconPictureBox1";
+            iconPictureBox1.Size = new Size(115, 115);
+            iconPictureBox1.TabIndex = 2;
+            iconPictureBox1.TabStop = false;
+            // 
+            // txtdocumento
+            // 
+            txtdocumento.Location = new Point(246, 41);
+            txtdocumento.Name = "txtdocumento";
+            txtdocumento.Size = new Size(204, 23);
+            txtdocumento.TabIndex = 3;
+            // 
+            // txtclave
+            // 
+            txtclave.Location = new Point(246, 96);
+            txtclave.Name = "txtclave";
+            txtclave.PasswordChar = '*';
+            txtclave.Size = new Size(204, 23);
+            txtclave.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(246, 23);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Nro Documento";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(246, 78);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Contraseña";
+            // 
+            // btningresar
+            // 
+            btningresar.BackColor = Color.RoyalBlue;
+            btningresar.Cursor = Cursors.Hand;
+            btningresar.FlatAppearance.BorderColor = Color.Black;
+            btningresar.FlatStyle = FlatStyle.Flat;
+            btningresar.ForeColor = Color.White;
+            btningresar.IconChar = FontAwesome.Sharp.IconChar.DoorOpen;
+            btningresar.IconColor = Color.White;
+            btningresar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btningresar.IconSize = 21;
+            btningresar.Location = new Point(227, 137);
+            btningresar.Name = "btningresar";
+            btningresar.Size = new Size(112, 41);
+            btningresar.TabIndex = 7;
+            btningresar.Text = "Ingresar";
+            btningresar.TextAlign = ContentAlignment.MiddleRight;
+            btningresar.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btningresar.UseVisualStyleBackColor = false;
+            btningresar.Click += btningresar_Click;
+            // 
+            // btncancelar
+            // 
+            btncancelar.BackColor = Color.Firebrick;
+            btncancelar.Cursor = Cursors.Hand;
+            btncancelar.FlatAppearance.BorderColor = Color.Black;
+            btncancelar.FlatStyle = FlatStyle.Flat;
+            btncancelar.ForeColor = Color.White;
+            btncancelar.IconChar = FontAwesome.Sharp.IconChar.StopCircle;
+            btncancelar.IconColor = Color.White;
+            btncancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btncancelar.IconSize = 21;
+            btncancelar.Location = new Point(354, 137);
+            btncancelar.Name = "btncancelar";
+            btncancelar.Size = new Size(118, 41);
+            btncancelar.TabIndex = 8;
+            btncancelar.Text = "Cancelar";
+            btncancelar.TextAlign = ContentAlignment.MiddleRight;
+            btncancelar.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btncancelar.UseVisualStyleBackColor = false;
+            btncancelar.Click += btncancelar_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(502, 193);
+            Controls.Add(btncancelar);
+            Controls.Add(btningresar);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(txtclave);
+            Controls.Add(txtdocumento);
+            Controls.Add(iconPictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private TextBox txtdocumento;
+        private TextBox txtclave;
+        private Label label3;
+        private Label label4;
+        private FontAwesome.Sharp.IconButton btningresar;
+        private FontAwesome.Sharp.IconButton btncancelar;
+    }
+}
