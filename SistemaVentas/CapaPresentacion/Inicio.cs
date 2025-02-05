08a40bc805c3b6dcc5637eee2a84f6ddb526a84e
Juan <juancruzrodriguez3@gmail.com>
2025-02-05 17:23:07 -0300

    Arreglo de conexion de base de datos

--- SistemaVentas/CapaPresentacion/Inicio.cs
+++ SistemaVentas/CapaPresentacion/Inicio.cs
@@ -2,9 +2,15 @@ namespace CapaPresentacion
 {
     public partial class Inicio : Form
     {
+
         public Inicio()
         {
             InitializeComponent();
+        }
+
+        private void lblUsuario_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
