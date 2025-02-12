0654fd5405db880a8ce72282302b2474ec21e5a9
Juan <juancruzrodriguez3@gmail.com>
2025-02-12 17:13:31 -0300

    form cliente terminado

--- SistemaVentas/CapaPresentacion/frmClientes.Designer.cs
+++ SistemaVentas/CapaPresentacion/frmClientes.Designer.cs
@@ -28,33 +28,462 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            btnbuscar = new FontAwesome.Sharp.IconButton();
+            dgvdata = new DataGridView();
+            btnseleccionar = new DataGridViewButtonColumn();
+            Id = new DataGridViewTextBoxColumn();
+            Documento = new DataGridViewTextBoxColumn();
+            NombreCompleto = new DataGridViewTextBoxColumn();
+            Correo = new DataGridViewTextBoxColumn();
+            Telefono = new DataGridViewTextBoxColumn();
+            EstadoValor = new DataGridViewTextBoxColumn();
+            Estado = new DataGridViewTextBoxColumn();
+            btnlimpiarbuscador = new FontAwesome.Sharp.IconButton();
+            txtbusqueda = new TextBox();
+            cbobusqueda = new ComboBox();
+            label11 = new Label();
+            txtid = new TextBox();
+            label10 = new Label();
+            txtindice = new TextBox();
+            label9 = new Label();
+            btneliminar = new FontAwesome.Sharp.IconButton();
+            btnlimpiar = new FontAwesome.Sharp.IconButton();
+            btnguardar = new FontAwesome.Sharp.IconButton();
+            label8 = new Label();
+            cboestado = new ComboBox();
+            txttelefono = new TextBox();
+            label5 = new Label();
+            txtcorreo = new TextBox();
+            txtnombrecompleto = new TextBox();
+            txtdocumento = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
             label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvdata).BeginInit();
             SuspendLayout();
+            // 
+            // btnbuscar
+            // 
+            btnbuscar.BackColor = Color.White;
+            btnbuscar.Cursor = Cursors.Hand;
+            btnbuscar.FlatAppearance.BorderColor = Color.Black;
+            btnbuscar.FlatStyle = FlatStyle.Flat;
+            btnbuscar.ForeColor = Color.Black;
+            btnbuscar.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlass;
+            btnbuscar.IconColor = Color.Black;
+            btnbuscar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnbuscar.IconSize = 16;
+            btnbuscar.Location = new Point(951, 47);
+            btnbuscar.Name = "btnbuscar";
+            btnbuscar.Size = new Size(44, 23);
+            btnbuscar.TabIndex = 53;
+            btnbuscar.UseVisualStyleBackColor = false;
+            btnbuscar.Click += btnbuscar_Click;
+            // 
+            // dgvdata
+            // 
+            dgvdata.AllowUserToAddRows = false;
+            dgvdata.BackgroundColor = Color.White;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.Padding = new Padding(2);
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvdata.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvdata.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvdata.Columns.AddRange(new DataGridViewColumn[] { btnseleccionar, Id, Documento, NombreCompleto, Correo, Telefono, EstadoValor, Estado });
+            dgvdata.Location = new Point(273, 97);
+            dgvdata.MultiSelect = false;
+            dgvdata.Name = "dgvdata";
+            dgvdata.ReadOnly = true;
+            dataGridViewCellStyle2.SelectionBackColor = Color.White;
+            dataGridViewCellStyle2.SelectionForeColor = Color.Black;
+            dgvdata.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            dgvdata.RowTemplate.Height = 28;
+            dgvdata.Size = new Size(842, 406);
+            dgvdata.TabIndex = 47;
+            dgvdata.CellContentClick += dgvdata_CellContentClick;
+            dgvdata.CellPainting += dgvdata_CellPainting;
+            // 
+            // btnseleccionar
+            // 
+            btnseleccionar.HeaderText = "";
+            btnseleccionar.Name = "btnseleccionar";
+            btnseleccionar.ReadOnly = true;
+            btnseleccionar.Width = 30;
+            // 
+            // Id
+            // 
+            Id.HeaderText = "Id";
+            Id.Name = "Id";
+            Id.ReadOnly = true;
+            Id.Visible = false;
+            // 
+            // Documento
+            // 
+            Documento.HeaderText = "Nro Documento";
+            Documento.Name = "Documento";
+            Documento.ReadOnly = true;
+            Documento.Width = 150;
+            // 
+            // NombreCompleto
+            // 
+            NombreCompleto.HeaderText = "Nombre Completo";
+            NombreCompleto.Name = "NombreCompleto";
+            NombreCompleto.ReadOnly = true;
+            NombreCompleto.Width = 180;
+            // 
+            // Correo
+            // 
+            Correo.HeaderText = "Correo";
+            Correo.Name = "Correo";
+            Correo.ReadOnly = true;
+            Correo.Width = 150;
+            // 
+            // Telefono
+            // 
+            Telefono.HeaderText = "Telefono";
+            Telefono.Name = "Telefono";
+            Telefono.ReadOnly = true;
+            // 
+            // EstadoValor
+            // 
+            EstadoValor.HeaderText = "EstadoValor";
+            EstadoValor.Name = "EstadoValor";
+            EstadoValor.ReadOnly = true;
+            EstadoValor.Visible = false;
+            // 
+            // Estado
+            // 
+            Estado.HeaderText = "Estado";
+            Estado.Name = "Estado";
+            Estado.ReadOnly = true;
+            // 
+            // btnlimpiarbuscador
+            // 
+            btnlimpiarbuscador.BackColor = Color.White;
+            btnlimpiarbuscador.Cursor = Cursors.Hand;
+            btnlimpiarbuscador.FlatAppearance.BorderColor = Color.Black;
+            btnlimpiarbuscador.FlatStyle = FlatStyle.Flat;
+            btnlimpiarbuscador.ForeColor = Color.Black;
+            btnlimpiarbuscador.IconChar = FontAwesome.Sharp.IconChar.Broom;
+            btnlimpiarbuscador.IconColor = Color.Black;
+            btnlimpiarbuscador.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnlimpiarbuscador.IconSize = 18;
+            btnlimpiarbuscador.Location = new Point(1001, 47);
+            btnlimpiarbuscador.Name = "btnlimpiarbuscador";
+            btnlimpiarbuscador.Size = new Size(38, 23);
+            btnlimpiarbuscador.TabIndex = 54;
+            btnlimpiarbuscador.TextAlign = ContentAlignment.MiddleRight;
+            btnlimpiarbuscador.UseVisualStyleBackColor = false;
+            btnlimpiarbuscador.Click += btnlimpiarbuscador_Click;
+            // 
+            // txtbusqueda
+            // 
+            txtbusqueda.Location = new Point(764, 48);
+            txtbusqueda.Name = "txtbusqueda";
+            txtbusqueda.Size = new Size(181, 23);
+            txtbusqueda.TabIndex = 52;
+            // 
+            // cbobusqueda
+            // 
+            cbobusqueda.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbobusqueda.FormattingEnabled = true;
+            cbobusqueda.Location = new Point(630, 48);
+            cbobusqueda.Name = "cbobusqueda";
+            cbobusqueda.Size = new Size(128, 23);
+            cbobusqueda.TabIndex = 51;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.White;
+            label11.Location = new Point(558, 53);
+            label11.Name = "label11";
+            label11.Size = new Size(66, 15);
+            label11.TabIndex = 50;
+            label11.Text = "Buscar por:";
+            // 
+            // txtid
+            // 
+            txtid.Location = new Point(176, 50);
+            txtid.Name = "txtid";
+            txtid.Size = new Size(25, 23);
+            txtid.TabIndex = 49;
+            txtid.Text = "0";
+            txtid.Visible = false;
+            // 
+            // label10
+            // 
+            label10.BackColor = Color.White;
+            label10.BorderStyle = BorderStyle.FixedSingle;
+            label10.Font = new Font("Verdana", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label10.Location = new Point(273, 24);
+            label10.Name = "label10";
+            label10.Padding = new Padding(2, 0, 0, 0);
+            label10.Size = new Size(842, 61);
+            label10.TabIndex = 48;
+            label10.Text = "Lista de Clientes";
+            label10.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // txtindice
+            // 
+            txtindice.Location = new Point(145, 50);
+            txtindice.Name = "txtindice";
+            txtindice.Size = new Size(25, 23);
+            txtindice.TabIndex = 55;
+            txtindice.Text = "-1";
+            txtindice.Visible = false;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.White;
+            label9.Font = new Font("Verdana", 14.25F, FontStyle.Italic, GraphicsUnit.Point);
+            label9.ForeColor = SystemColors.ControlText;
+            label9.Location = new Point(18, 24);
+            label9.Name = "label9";
+            label9.Size = new Size(146, 23);
+            label9.TabIndex = 46;
+            label9.Text = "Detalle Cliente";
+            // 
+            // btneliminar
+            // 
+            btneliminar.BackColor = Color.Firebrick;
+            btneliminar.Cursor = Cursors.Hand;
+            btneliminar.FlatAppearance.BorderColor = Color.Black;
+            btneliminar.FlatStyle = FlatStyle.Flat;
+            btneliminar.ForeColor = Color.White;
+            btneliminar.IconChar = FontAwesome.Sharp.IconChar.TrashAlt;
+            btneliminar.IconColor = Color.White;
+            btneliminar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btneliminar.IconSize = 16;
+            btneliminar.Location = new Point(18, 373);
+            btneliminar.Name = "btneliminar";
+            btneliminar.Size = new Size(183, 23);
+            btneliminar.TabIndex = 45;
+            btneliminar.Text = "Eliminar";
+            btneliminar.TextAlign = ContentAlignment.MiddleRight;
+            btneliminar.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btneliminar.UseVisualStyleBackColor = false;
+            btneliminar.Click += btneliminar_Click;
+            // 
+            // btnlimpiar
+            // 
+            btnlimpiar.BackColor = Color.RoyalBlue;
+            btnlimpiar.Cursor = Cursors.Hand;
+            btnlimpiar.FlatAppearance.BorderColor = Color.Black;
+            btnlimpiar.FlatStyle = FlatStyle.Flat;
+            btnlimpiar.ForeColor = Color.White;
+            btnlimpiar.IconChar = FontAwesome.Sharp.IconChar.Broom;
+            btnlimpiar.IconColor = Color.White;
+            btnlimpiar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnlimpiar.IconSize = 18;
+            btnlimpiar.Location = new Point(18, 344);
+            btnlimpiar.Name = "btnlimpiar";
+            btnlimpiar.Size = new Size(183, 23);
+            btnlimpiar.TabIndex = 44;
+            btnlimpiar.Text = "Limpiar";
+            btnlimpiar.TextAlign = ContentAlignment.MiddleRight;
+            btnlimpiar.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnlimpiar.UseVisualStyleBackColor = false;
+            btnlimpiar.Click += btnlimpiar_Click;
+            // 
+            // btnguardar
+            // 
+            btnguardar.BackColor = Color.ForestGreen;
+            btnguardar.Cursor = Cursors.Hand;
+            btnguardar.FlatAppearance.BorderColor = Color.Black;
+            btnguardar.FlatStyle = FlatStyle.Flat;
+            btnguardar.ForeColor = Color.White;
+            btnguardar.IconChar = FontAwesome.Sharp.IconChar.Folder;
+            btnguardar.IconColor = Color.White;
+            btnguardar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnguardar.IconSize = 16;
+            btnguardar.Location = new Point(18, 315);
+            btnguardar.Name = "btnguardar";
+            btnguardar.Size = new Size(183, 23);
+            btnguardar.TabIndex = 43;
+            btnguardar.Text = "Guardar";
+            btnguardar.TextAlign = ContentAlignment.MiddleRight;
+            btnguardar.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnguardar.UseVisualStyleBackColor = false;
+            btnguardar.Click += btnguardar_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.White;
+            label8.Location = new Point(18, 258);
+            label8.Name = "label8";
+            label8.Size = new Size(48, 15);
+            label8.TabIndex = 42;
+            label8.Text = "Estado: ";
+            // 
+            // cboestado
+            // 
+            cboestado.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboestado.FormattingEnabled = true;
+            cboestado.Location = new Point(18, 276);
+            cboestado.Name = "cboestado";
+            cboestado.Size = new Size(183, 23);
+            cboestado.TabIndex = 41;
+            // 
+            // txttelefono
+            // 
+            txttelefono.Location = new Point(18, 223);
+            txttelefono.Name = "txttelefono";
+            txttelefono.Size = new Size(183, 23);
+            txttelefono.TabIndex = 36;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.White;
+            label5.Location = new Point(18, 205);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 15);
+            label5.TabIndex = 35;
+            label5.Text = "Teléfono:";
+            // 
+            // txtcorreo
+            // 
+            txtcorreo.Location = new Point(18, 171);
+            txtcorreo.Name = "txtcorreo";
+            txtcorreo.Size = new Size(183, 23);
+            txtcorreo.TabIndex = 34;
+            // 
+            // txtnombrecompleto
+            // 
+            txtnombrecompleto.Location = new Point(18, 126);
+            txtnombrecompleto.Name = "txtnombrecompleto";
+            txtnombrecompleto.Size = new Size(183, 23);
+            txtnombrecompleto.TabIndex = 33;
+            // 
+            // txtdocumento
+            // 
+            txtdocumento.Location = new Point(18, 79);
+            txtdocumento.Name = "txtdocumento";
+            txtdocumento.Size = new Size(183, 23);
+            txtdocumento.TabIndex = 32;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.White;
+            label4.Location = new Point(18, 153);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 15);
+            label4.TabIndex = 31;
+            label4.Text = "Correo:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.White;
+            label3.Location = new Point(18, 108);
+            label3.Name = "label3";
+            label3.Size = new Size(110, 15);
+            label3.TabIndex = 30;
+            label3.Text = "Nombre Completo:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Location = new Point(18, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 15);
+            label2.TabIndex = 29;
+            label2.Text = "Nro Documento:";
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(305, 199);
+            label1.BackColor = Color.White;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Dock = DockStyle.Left;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new Size(79, 25);
-            label1.TabIndex = 2;
-            label1.Text = "Clientes";
+            label1.Size = new Size(253, 564);
+            label1.TabIndex = 28;
             // 
             // frmClientes
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1197, 564);
+            Controls.Add(btnbuscar);
+            Controls.Add(dgvdata);
+            Controls.Add(btnlimpiarbuscador);
+            Controls.Add(txtbusqueda);
+            Controls.Add(cbobusqueda);
+            Controls.Add(label11);
+            Controls.Add(txtid);
+            Controls.Add(label10);
+            Controls.Add(txtindice);
+            Controls.Add(label9);
+            Controls.Add(btneliminar);
+            Controls.Add(btnlimpiar);
+            Controls.Add(btnguardar);
+            Controls.Add(label8);
+            Controls.Add(cboestado);
+            Controls.Add(txttelefono);
+            Controls.Add(label5);
+            Controls.Add(txtcorreo);
+            Controls.Add(txtnombrecompleto);
+            Controls.Add(txtdocumento);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
             Name = "frmClientes";
             Text = "frmClientes";
+            Load += frmClientes_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvdata).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
+        private FontAwesome.Sharp.IconButton btnbuscar;
+        private DataGridView dgvdata;
+        private FontAwesome.Sharp.IconButton btnlimpiarbuscador;
+        private TextBox txtbusqueda;
+        private ComboBox cbobusqueda;
+        private Label label11;
+        private TextBox txtid;
+        private Label label10;
+        private TextBox txtindice;
+        private Label label9;
+        private FontAwesome.Sharp.IconButton btneliminar;
+        private FontAwesome.Sharp.IconButton btnlimpiar;
+        private FontAwesome.Sharp.IconButton btnguardar;
+        private Label label8;
+        private ComboBox cboestado;
+        private TextBox txttelefono;
+        private Label label5;
+        private TextBox txtcorreo;
+        private TextBox txtnombrecompleto;
+        private TextBox txtdocumento;
+        private Label label4;
+        private Label label3;
+        private Label label2;
         private Label label1;
+        private DataGridViewButtonColumn btnseleccionar;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn Documento;
+        private DataGridViewTextBoxColumn NombreCompleto;
+        private DataGridViewTextBoxColumn Correo;
+        private DataGridViewTextBoxColumn Telefono;
+        private DataGridViewTextBoxColumn EstadoValor;
+        private DataGridViewTextBoxColumn Estado;
     }
 }
